6cc9e29e35010536e36054d0c429a76829537d0b
Mou <m.mounir.f@gmail.com>
2024-03-23 20:01:24 +0200

    Move DrawingUtils to useCanvas to simplify consumer implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,14 @@
-import { GestureRecognizer } from "@mediapipe/tasks-vision";
-import { useEffect } from "react";
 import Webcam from "react-webcam";
 import useCanvas from "./hooks/useCanvas";
 import useRecognizer from "./hooks/useRecognizer";
 
 function App() {
   const { videoRef, results } = useRecognizer();
-  const { canvasRef, drawingUtils } = useCanvas();
+  const { canvasRef } = useCanvas(results?.landmarks);
 
   const onUserMediaError = (error: string | DOMException) => {
     console.error("Error getting user media", error);
   };
-
-  useEffect(() => {
-    if (canvasRef.current && drawingUtils && results) {
-      const canvasContext = canvasRef.current.getContext("2d");
-
-      if (canvasContext) {
-        canvasContext.save();
-        canvasContext.clearRect(
-          0,
-          0,
-          canvasRef.current.width,
-          canvasRef.current.height,
-        );
-        canvasContext.restore();
-
-        if (results?.landmarks) {
-          for (const landmarks of results.landmarks) {
-            drawingUtils.drawConnectors(
-              landmarks,
-              GestureRecognizer.HAND_CONNECTIONS,
-              {
-                color: "#12B886",
-                lineWidth: 2,
-              },
-            );
-            drawingUtils.drawLandmarks(landmarks, {
-              color: "#12B886",
-              radius: 3,
-            });
-            drawingUtils.drawLandmarks(landmarks, {
-              color: "#94D82D",
-              radius: 1,
-            });
-          }
-        }
-      }
-    }
-  }, [results, drawingUtils, canvasRef]);
 
   return (
     <main>
--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -1,13 +1,13 @@
-import { DrawingUtils } from '@mediapipe/tasks-vision';
-import { useRef, useState } from 'react';
+import { DrawingUtils, GestureRecognizer, type NormalizedLandmark } from '@mediapipe/tasks-vision';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useEffectOnce } from './useEffectOnce';
 
-export default function useCanvas() {
+export default function useCanvas(landmarksArray?: NormalizedLandmark[][]) {
   const [drawingUtils, setDrawingUtils] = useState<DrawingUtils>();
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const initializeDrawingUtils = async () => {
-    if (!canvasRef.current || drawingUtils) return;
+    if (!canvasRef.current) return;
     const canvasContext = canvasRef.current.getContext('2d');
 
     if (!canvasContext) {
@@ -18,5 +18,43 @@ export default function useCanvas() {
 
   useEffectOnce(initializeDrawingUtils);
 
-  return { drawingUtils, canvasRef };
+  const draw = useCallback(
+    (landmarks: NormalizedLandmark[][]) => {
+      if (!canvasRef.current || !drawingUtils) return;
+      const canvasContext = canvasRef.current.getContext('2d');
+      if (!canvasContext) {
+        throw new Error('Canvas context not found.');
+      }
+
+      canvasContext.save();
+      canvasContext.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+      canvasContext.restore();
+
+      if (landmarks) {
+        for (const landmark of landmarks) {
+          drawingUtils.drawConnectors(landmark, GestureRecognizer.HAND_CONNECTIONS, {
+            color: '#12B886',
+            lineWidth: 2,
+          });
+          drawingUtils.drawLandmarks(landmark, {
+            color: '#12B886',
+            radius: 3,
+          });
+          drawingUtils.drawLandmarks(landmark, {
+            color: '#94D82D',
+            radius: 1,
+          });
+        }
+      }
+    },
+    [drawingUtils]
+  );
+
+  useEffect(() => {
+    if (landmarksArray) {
+      draw(landmarksArray);
+    }
+  }, [draw, landmarksArray]);
+
+  return { landmarksArray, canvasRef };
 }
