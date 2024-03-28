b8a881feb175c60e45a9a5a28249cab867490c34
Mou <m.mounir.f@gmail.com>
2024-03-28 07:12:19 +0200

    Fix base path on deployed github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/react-hand-gesture-recognition/",
 	build: {
 		target: "ESNext",
 	},
